d22c81600a0abae86fd6c42004cd219052ef569b
tahab <bezcitaha@gmail.com>
2025-09-23 17:44:43 +0300

    feat: Firestore veri ekleme, okuma, guncelleme ve silme islevleri entegre edildi

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
-
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -22,5 +22,6 @@ const analytics = getAnalytics(app);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+export const db = getFirestore(app);
 
 export default app;
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,135 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { collection, addDoc, getDocs,doc, deleteDoc, updateDoc } from "firebase/firestore"; 
+import { db } from '../../firebaseConfig';
+import CustomButton from '../components/CustomButton';
 
 const HomePage = () => {
+
+  // firestore'dan çekilen veriler
+  const [data, setData] = useState([])
+  // veri kaydedildi mi durumu
+  const [isSaved, setIsSaved] = useState(false)
+  // güncelleme için state
+  const [updetTheData, setUpdetTheData] = useState('')
+
+  console.log(isSaved)
+
+  // Sayfa yüklendiğinde verileri çek
+  useEffect(() => {
+    getData();
+  }, [isSaved]) // isSaved durumu değiştiğinde verileri tekrar çek
+
+  // Firestore'a veri ekleme fonksiyonu
+  const sendData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "reactNativeLesson"), {
+        title: "Zero to Hero",
+        content: "React Native ile mobil uygulama geliştirme",
+        lesson: 100
+    });
+    console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  // Firestore'dan veri okuma fonksiyonu
+  const getData = async () => {
+    const allData = [];
+
+    try {
+      const querySnapshot = await getDocs(collection(db, "reactNativeLesson"));
+        querySnapshot.forEach((doc) => {
+        //console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        //verileri diziye ekleme
+        allData.push({id: doc.id, ...doc.data()})
+      });
+      setData(allData);
+    } catch (error) {
+      console.log("Error getting documents: ", error);
+    }
+  }
+  
+  // Firestore'dan veri silme fonksiyonu
+  const deleteData = async (value) => {
+    try {
+      await deleteDoc(doc(db, "reactNativeLesson", value));
+      console.log("Document successfully deleted!");
+    } catch (error) {
+      console.error("Error removing document: ", error);
+      
+    }
+  }
+
+  // Firestore'da veri güncelleme fonksiyonu
+  const updateData = async (value) => {
+    try {
+      const docRef = doc(db, "reactNativeLesson", value);
+      await updateDoc(docRef, {
+        content: updetTheData
+      });
+    } catch (error) {
+      
+    }
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Text>HomePage</Text>
+
+      <TextInput
+        value={updateData}
+        onChangeText={setUpdetTheData}
+        placeholder='veri girin'
+        style={{width: "80%", height: 50, backgroundColor: "lightblue", marginBottom: 20, paddingHorizontal: 10, borderRadius: 20}}
+      />
+      
+      {data.map((value,index) => {
+        return (
+          <Pressable 
+            onPress={()=> [updateData(value.id), setIsSaved(isSaved===false ? true : false)]}
+            key={index}>
+            <Text>{index}</Text>
+            <Text>{value.title}</Text>
+            <Text>{value.content}</Text>
+            <Text>{value.lesson}</Text>
+          </Pressable>
+        )
+      })}
+
+      
+      <CustomButton
+      title={"kaydet"}
+      setWidth="40%"
+      buttonColor="lightblue"
+      buttonPressedColor="darkcyan"
+      onPress={()=> {sendData(), setIsSaved(isSaved===false ? true : false)}}
+      />
+
+      <CustomButton
+      title={"verileri getir"}
+      setWidth="40%"
+      buttonColor="lightblue"
+      buttonPressedColor="darkcyan"
+      onPress={getData}
+      />
+
+      <CustomButton
+      title={"verileri sil"}
+      setWidth="40%"
+      buttonColor="lightblue"
+      buttonPressedColor="darkcyan"
+      onPress={deleteData}
+      />
+
+      <CustomButton
+      title={"verileri güncelle"}
+      setWidth="40%"
+      buttonColor="lightblue"
+      buttonPressedColor="darkcyan"
+      onPress={updateData}
+      />
     </View>
   )
 }
@@ -16,5 +141,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#dde2f8ff',
   }
 })
